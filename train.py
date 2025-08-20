b382693c931a3a2a2e5fd8f83056285744697494
Jim Dowling <jim@hopsworks.ai>
2025-08-20 07:08:33 +0000

    scaleouts model

--- train.py
+++ train.py
@@ -1,15 +1,16 @@
 import argparse
 from ultralytics import YOLO
 import torch.multiprocessing as mp
+import os
 mp.set_sharing_strategy("file_system")
-
+os.environ["TORCH_SHARE_DIR"] = "/tmp/yolov8-face"
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', default='yolov8n.pt', help='Initial weights path')
     parser.add_argument('--data', default='data/widerface.yaml', help='Path to data file')
     parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch', type=int, default=16)
+    parser.add_argument('--batch', type=int, default=32)
     parser.add_argument('--imgsz', type=int, default=640, help='Image size')
     parser.add_argument('--device', default='0', help='CUDA device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--resume', type=bool, nargs='?', const=True, default=False, help='Resume most recent training')
@@ -27,7 +28,10 @@ if __name__ == '__main__':
         'device': opt.device,
         'resume': opt.resume,
         'workers': opt.workers,
+        'cache': "ram",
+        'amp': True,
     }
+    
     if opt.optimizer:
         params['optimizer'] = opt.optimizer
     if opt.lrf:
